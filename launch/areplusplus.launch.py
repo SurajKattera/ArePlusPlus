0ccda957364945ca7b1f0d9b1a3a47a5ff6e4454
Dinh Bach Nguyen <89898151+bachnguyen3005@users.noreply.github.com>
2024-10-27 18:38:39 +1100

    Update areplusplus.launch.py

--- launch/areplusplus.launch.py
+++ launch/areplusplus.launch.py
@@ -14,5 +14,21 @@ def generate_launch_description():
             executable='artag_detector_node',
             name='artag_detector_node',
             output='screen'
+        ), 
+        # Launch AprilTag Node from apriltag_ros package
+        Node(
+            package='apriltag_ros',
+            executable='apriltag_node',
+            name='apriltag_node',
+            output='screen',
+            remappings=[
+                ('image_rect', '/camera/image_raw'),
+                ('camera_info', '/camera/camera_info')
+            ],
+            parameters=[
+                {'family': '36h11'},
+                {'size': 0.5},
+                {'max_hamming': 0}
+            ]
         )
     ])
